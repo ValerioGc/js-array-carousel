a2d3594d1b15656f7e162c539a5c81bb9070d2a4
ValerioGc <valerio.gc1@gmail.com>
2022-07-08 16:33:31 +0200

    Aggiunta classe show primo elemento

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // Array foto
-const img_arr = [   "img/01.jpg",
-                    "img/02.jpg",
-                    "img/03.jpg",
-                    "img/04.jpg",
-                    "img/05.jpg"
-                ];
+const img_arr = [   
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg"
+];
 let img_arr_l = img_arr.length;
 // Costruzione Dinamica HTML
 let carousel_cont = document.getElementById('img-carousel');
@@ -15,9 +16,20 @@ for (let i = 0; i < img_arr_l; i++) {
         </div>
     `;
 }
+const img = document.getElementsByClassName('img');
 
-
+let active = 0;
+img[active].classList.add('show');
 
 // Event Listner Back
+const back = document.getElementById('back');
+back.addEventListener('click',
+    function() {
 
+    });
 // Event Listner Forw
+const forw = document.getElementById('forw');
+forw.addEventListener('click',
+    function () {
+
+    });
