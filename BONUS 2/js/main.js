47411d17103151db24ed7cd7a327e6ea3dc6dce8
ValerioGc <valerio.gc1@gmail.com>
2022-07-10 21:44:08 +0200

    Aggiunta collegamento foto da preview

--- BONUS 2/js/main.js
+++ BONUS 2/js/main.js
@@ -61,4 +61,68 @@ back.addEventListener('click',
             back.classList.add('hidden');
         }
     });
-
+// Spostamento classe active prima foto
+let prevbtn1 = document.querySelector('.imgp:first-child');
+prevbtn1.addEventListener('click',
+function () {
+    // Spostamento classe active
+    img[active].classList.remove('show');
+    l_prev[active].classList.add('ovrl');
+    active = img_arr_l - 5;
+    forw.classList.remove('hidden');
+    back.classList.add('hidden');
+    img[active].classList.add('show');
+    l_prev[active].classList.remove('ovrl');
+});
+// Spostamento classe active seconda foto
+let prevbtn2 = document.querySelector('.imgp:nth-child(2)');
+prevbtn2.addEventListener('click',
+function () {
+    // Spostamento classe active
+    img[active].classList.remove('show');
+    l_prev[active].classList.add('ovrl');
+    active = img_arr_l - 4;
+    forw.classList.remove('hidden');
+    back.classList.remove('hidden');
+    img[active].classList.add('show');
+    l_prev[active].classList.remove('ovrl');
+});
+// Spostamento classe active terza foto
+let prevbtn3 = document.querySelector('.imgp:nth-child(3)');
+prevbtn3.addEventListener('click',
+function () {
+    // Spostamento classe active
+    img[active].classList.remove('show');
+    l_prev[active].classList.add('ovrl');
+    active = img_arr_l - 3;
+    forw.classList.remove('hidden');
+    back.classList.remove('hidden');
+    img[active].classList.add('show');
+    l_prev[active].classList.remove('ovrl');
+});
+// Spostamento classe active penultima foto
+let prevbtn4 = document.querySelector('.imgp:nth-child(4)');
+prevbtn4.addEventListener('click',
+function () {
+    // Spostamento classe active
+    img[active].classList.remove('show');
+    l_prev[active].classList.add('ovrl');
+    active = img_arr_l - 2;
+    forw.classList.remove('hidden');
+    back.classList.remove('hidden');
+    img[active].classList.add('show');
+    l_prev[active].classList.remove('ovrl');
+}); 
+// Spostamento classe active ultima foto
+let prevbtn5 = document.querySelector('.imgp:nth-child(5)');
+prevbtn5.addEventListener('click',
+function () {
+    // Spostamento classe active
+    img[active].classList.remove('show');
+    l_prev[active].classList.add('ovrl');
+    active = img_arr_l - 1;
+    forw.classList.add('hidden');
+    back.classList.remove('hidden');
+    img[active].classList.add('show');
+    l_prev[active].classList.remove('ovrl');
+});
