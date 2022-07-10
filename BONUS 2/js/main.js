36e371eb7e8c38b0a291147b0266b3c270393d07
ValerioGc <valerio.gc1@gmail.com>
2022-07-10 20:27:43 +0200

    Bonus 3

--- BONUS 2/js/main.js
+++ BONUS 2/js/main.js
@@ -8,6 +8,9 @@ const img_arr = [
 ];
 let img_arr_l = parseInt(img_arr.length);
 // Costruzione Dinamica HTML 
+const img = document.getElementsByClassName('img');
+const cont_l_prev = document.getElementById('l-prev');
+const l_prev = document.getElementsByClassName('imgp');
 let carousel_cont = document.getElementById('img-carousel');
 let preview = document.getElementById('ao');
 for (let i = 0; i < img_arr_l; i++) {
@@ -16,15 +19,15 @@ for (let i = 0; i < img_arr_l; i++) {
             <img src=${img_arr[i]} alt="Immagine ${i + 1}">
         </li>
     `;
-    preview.innerHTML +=
-        `   <li class="imgp">
+    cont_l_prev.innerHTML +=
+        `   <li class="imgp ovrl">
                 <img src=${img_arr[i]} alt="Immagine ${i + 1}">
             </li>
     `;
 }
-const img = document.getElementsByClassName('img');
 let active = 0;
 img[active].classList.add('show');
+l_prev[active].classList.remove('ovrl');
 // EventListners
 const forw = document.getElementById('forw');
 const back = document.getElementById('back');
@@ -33,8 +36,10 @@ forw.addEventListener('click',
     function () {
         // Spostamento classe
         img[active].classList.remove('show');
+        l_prev[active].classList.add('ovrl');
         active++;
         img[active].classList.add('show');
+        l_prev[active].classList.remove('ovrl');
         // Rimozione bottone avanti
         back.classList.remove('hidden');
         if (active == img_arr_l - 1 ) {
@@ -46,8 +51,10 @@ back.addEventListener('click',
     function () {
         // Spostamento classe
         img[active].classList.remove('show');
+        l_prev[active].classList.add('ovrl');
         active--;
         img[active].classList.add('show');
+        l_prev[active].classList.remove('ovrl');
         // Rimozione bottone indietro
         forw.classList.remove('hidden');
         if (active == 0 ) {
